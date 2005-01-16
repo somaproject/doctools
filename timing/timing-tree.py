e484fc152e0f9b39d544be7929edc29ff6390791
silvan@MIT.EDU <silvan@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2005-01-16 00:23:41 +0000

    Latest version, with more working bits.
    Specifically, the program has been expanded to add signals and buses into the tree; breaks in the signal have yet to be implemented.  Also changed the xml file to test this program.
    
    
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/docs/tools@869 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- timing/timing-tree.py
+++ timing/timing-tree.py
@@ -73,9 +73,9 @@ class TimingHandler(ContentHandler):
             if name == "clock":
                 self.timingobject.add_clock_to_tree(self.name, self.datastr)
             elif name == "signal":
-                self.timingobject.add_signal(self.name, self.datastr)
+                self.timingobject.add_signal_to_tree(self.name, self.datastr)
             elif name == "bus":                
-                self.timingobject.add_bus(self.name, self.datastr, self.classstr)
+                self.timingobject.add_bus_to_tree(self.name, self.datastr, self.classstr)
 
             if name == "timing" or name=="timingobject":
                 self.inTiming = False
@@ -113,11 +113,64 @@ class timing:
 	self.cycles = len(datastr.split())
 	clksig = signal(name, self.xzero, self.ypos, self.period, self.height)
 	tree.append(self.cycles)
-	tree.append((clksig.draw_name(), clksig.draw_clock()))
+	tree.append((clksig.create_name_node(), clksig.create_clock_node()))
 	
 	self.ypos += self.signalspacing + self.height  
 
+    def add_signal_to_tree(self, name, datastr):
+	sig = signal(name, self.xzero, self.ypos, self.period, self.height)
+	sigelem = []	
+        for i in datastr.split():
+            if i == 'H':
+                sigelem.append(sig.create_high_node())
+            elif i == 'L':
+                sigelem.append(sig.create_low_node())
+            elif i == 'Z':
+                sigelem.append(sig.create_z_node())
+            elif i == '//':
+                sigelem.append(sig.draw_split())
 
+        self.ypos += self.signalspacing + self.height   
+#        self.signalselem.append(sigelem)
+        self.signalcnt += 1	
+	tree.append(sig.create_name_node()) 
+	tree.append(sigelem)
+
+    def add_bus_to_tree(self, name, datastr, classstr):
+        sigelem = []
+        
+        sig = signal(name, self.xzero, self.ypos, self.period, self.height)
+
+        color = "white"
+
+        data = datastr.split()
+
+        if classstr != None:
+            classes = classstr.split()
+
+        
+        for i in range(len(data)):
+            cyccolor = color
+            if len(classes) == 0:
+                cl = 0
+            else:
+                cl = classes[i]
+            if self.classes.has_key(cl):
+                cyccolor = self.classes[cl]
+            else:
+                cyccolor = self.colors.pop(0)
+                self.classes[cl] = cyccolor
+            if data[i] =='//':
+                sigelem.append(sig.draw_split())
+            elif data[i] == 'Z':
+                sigelem.append(sig.create_z_node())
+            else:
+                sigelem.append(sig.create_bus_node(data[i], cyccolor))            
+        self.ypos += self.signalspacing + self.height   
+#        self.signalselem.append(sigelem)
+	tree.append(sig.create_name_node())
+	tree.append(sigelem)
+        self.signalcnt += 1	
 
 class signal:
 
@@ -133,7 +186,7 @@ class signal:
         self.sval = ''   # previous value
 
 
-    def draw_name(self):
+    def create_name_node(self):
         """ the name is to the left of the original draw point"""
         dict = {}
         dict['x'] = (self.startx -4)
@@ -144,7 +197,7 @@ class signal:
         dict['name'] = self.name
         return dict
       
-    def draw_clock(self):
+    def create_clock_node(self):
 	dict = {}
         ptuple = (self.x, self.y, self.period/2, -self.height, self.period/2, self.height)
         dict['d'] = "M%f,%f h%f v%f h%f v%f" % ptuple
@@ -155,10 +208,96 @@ class signal:
         self.sval = 'L'
 	return dict
 
+    def create_low_node(self):
+	dict = {}
 
+        if self.sval == 'H':
+            starty = self.y - self.height
+        elif self.sval == 'Z':
+            starty = self.y - self.height/2
+        else:
+            starty = self.y
 
+        ptuple = [self.x, starty,  self.period/8, self.y-starty, 7*self.period/8]
+        dict['d'] = ptuple
+        dict['stroke'] = "black"
+        dict['fill'] = "none"
+        dict['stroke-linecap'] = "square"
+        self.x += self.period
+        self.sval = 'L'
+	return dict
 
+    def create_high_node(self):
 
+        if self.sval == 'L':
+            starty = self.y
+            delta = -self.height
+        elif self.sval == 'Z':
+            starty = self.y - self.height/2
+            delta = -self.height/2
+        else:
+            starty = self.y - self.height
+            delta = 0.0
+	dict = {}
+        ptuple = [self.x, starty,  self.period/8, delta, 7*self.period/8]
+        dict['d'] = ptuple
+        dict['stroke'] = "black"
+        dict['fill'] = "none"
+        dict['stroke-linecap'] = "square"
+        self.x += self.period
+        self.sval = 'H'
+
+        return dict
+
+    def create_z_node(self):
+        dict = {}
+
+        if self.sval == 'L':
+            starty = self.y
+            delta = -self.height/2.0
+        elif self.sval == 'H':
+            starty = self.y - self.height
+            delta = self.height/2
+        else:
+            starty = self.y - self.height/2.0
+            delta = 0.0
+
+        ptuple = (self.x, starty,  self.period/8, delta, 7*self.period/8)
+        dict['d'] = "M%f,%f  l%f, %f h%f" % ptuple
+        dict['stroke'] = "black"
+        dict['fill'] = "none"
+        dict['stroke-linecap'] = "square"
+        self.x += self.period
+        self.sval = 'Z'
+        
+        return dict
+
+    def create_bus_node(self, title, color):
+
+        gelem = []
+
+        oelem = {}
+
+        otuple = (self.x, self.y - self.height/2, self.period/8, -self.height/2, 6*self.period/8.0, self.period/8.0, self.height/2, -self.period/8, self.height/2.0, -6*self.period/8.0)
+        oelem['d']= "M%f,%f l%f,%f h%f l%f,%f l%f,%f h%f Z" % otuple
+        oelem['stroke'] = "black"
+        oelem['fill'] = color
+        oelem['stroke-linecap'] = "square"
+
+        gelem.append(oelem)
+        
+        telem = {}
+        telem['x'] =  "%f" %  (self.x + self.period/2)
+        telem['y'] = "%f" % (self.y - self.height/4.0-0.3)
+        telem['font-family'] = "Helvetica"
+        telem['font-size'] = "%f" % (self.height/1.9)
+        telem['text-anchor'] = "middle"
+        telem['text'] = title
+        
+        gelem.append(telem)
+        self.x += self.period
+        self.sval = 'Z'
+        return gelem
 
 
 def main():
